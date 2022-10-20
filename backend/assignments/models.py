f945ee415c54742d0840509f32b8fd9fd1ac80ff
Nguyen Tuan Nam <tuannamnguyen290602@gmail.com>
2022-10-20 14:28:48 +0700

    sửa models

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -1,14 +1,14 @@
+from datetime import datetime
 from django.db import models
 
-
 class Assignments(models.Model):
-    courses = models.ForeignKey('courses.CoursesPerCycle', on_delete=models.CASCADE)
-    assignment_num = models.IntegerField()
-    assignment_datetime = models.DateTimeField()
+    courses = models.ForeignKey('courses.Courses', on_delete=models.CASCADE)
+    assignment_number = models.IntegerField(primary_key=True)
+    assignment_date = models.DateField(default=datetime.now)
+    assignment_deadline = models.DateTimeField()
     description = models.TextField()
 
-
 class Scores(models.Model):
-    assignments = models.ForeignKey(Assignments, on_delete=models.CASCADE)
+    assignments = models.ForeignKey('assignments.Assignments', on_delete=models.CASCADE)
     students = models.ForeignKey('users.Students', on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=4, decimal_places=2)
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -3,62 +3,45 @@ from django.db import models
 class Courses(models.Model):
     course_id = models.CharField(max_length=10, primary_key=True)
     course_description = models.CharField(max_length=100)
-    abstract = models.TextField()
-    required_textbook = models.TextField()
-
-class Cycle(models.Model):
-    cycle_id = models.CharField(max_length=10, primary_key=True)
-    cycle_description = models.CharField(max_length=100)
-    cycle_start_date = models.DateField()
-    cycle_end_date = models.DateField()
-
-
-class CoursesPerCycle(models.Model):
-    courses = models.ForeignKey('courses.Courses', on_delete=models.CASCADE)
-    cycles = models.ForeignKey('courses.Cycle', on_delete=models.CASCADE)
-    course_start_date = models.DateField()
-    course_end_date = models.DateField()
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint('courses', 'cycles', name='cpc_unique_courses_cycles')
-        ]
+    course_abstract = models.TextField()
+    course_required_textbook = models.TextField()
 
 class Enrollments(models.Model):
-    courses = models.ForeignKey('courses.CoursesPerCycle', on_delete=models.CASCADE)
+    courses = models.ForeignKey('courses.Courses', on_delete=models.CASCADE)
     students = models.ForeignKey('users.Students', on_delete=models.CASCADE)
     enrollment_date = models.DateField()
     cancelled = models.BooleanField()
-    cancelled_reason = models.CharField(max_length=100, blank=True)
+    cancelled_reason = models.CharField(max_length=100)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint('students', 'courses', name='enrollments_unique_courses_students')
+            models.UniqueConstraint('students', 'courses', name='enrollments_unique')
         ]
 
-
 class Classes(models.Model):
-    courses = models.ForeignKey('courses.CoursesPerCycle', on_delete=models.CASCADE)
+    courses = models.ForeignKey('courses.Courses', on_delete=models.CASCADE)
     teachers = models.ForeignKey('users.Teachers', on_delete=models.CASCADE)
-    class_no = models.IntegerField()
-    class_title = models.CharField(max_length=100)
-    class_date = models.CharField(max_length=100)
+    class_number = models.IntegerField(primary_key=True)
+    class_date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     class Meta:
         constraints = [
-            models.UniqueConstraint('courses', 'teachers', name='classes_unique_courses_teachers')
+            models.UniqueConstraint('courses', 'teachers', name='classes_unique')
         ]
-
 
 class Attendance(models.Model):
     classes = models.ForeignKey('courses.Classes', on_delete=models.CASCADE)
     students = models.ForeignKey('users.Students', on_delete=models.CASCADE)
-    time_arrive = models.TimeField(blank=True)
-    time_leave = models.TimeField(blank=True)
+    time_arrive = models.TimeField(blank=True, null=True)
+    time_leave = models.TimeField(blank=True, null=True)
 
     class Meta: 
         constraints = [
-            models.UniqueConstraint('classes', 'students', name='attendance_unique_classes_students')
+            models.UniqueConstraint('classes', 'students', name='attendance_unique')
         ]
+
+
+
+
