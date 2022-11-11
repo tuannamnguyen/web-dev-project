f2bed0b450d5f6a2a7649448195392dd01a06f82
bmson7112 <sonm6976@gmail.com>
2022-11-12 03:18:11 +0700

    them api signup, login, logout cho cac user

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,4 +1,4 @@
-from .models import Students, Teachers, TeachersPerCourse
+from .models import *
 from rest_framework import serializers
 
 
@@ -18,3 +18,64 @@ class TeachersPerCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeachersPerCourse
         fields = '__all__'
+
+
+
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['username', 'email', 'is_student','is_teacher']
+
+class StudentsSignupSerializer(serializers.ModelSerializer):
+    password2=serializers.CharField(style={"input_type":"password"}, write_only=True)
+    class Meta:
+        model=User
+        fields=['username','email','password', 'password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    
+    def save(self, **kwargs):
+        user=User(
+            username=self.validated_data['username'],
+            email=self.validated_data['email']
+        )
+        password=self.validated_data['password']
+        password2=self.validated_data['password2']
+        if password !=password2:
+            raise serializers.ValidationError({"error":"password do not match"})
+        user.set_password(password)
+        user.is_student=True
+        user.save()
+        Students.objects.create(user=user)
+        return user
+
+
+class TeachersSignupSerializer(serializers.ModelSerializer):
+    password2=serializers.CharField(style={"input_type":"password"}, write_only=True)
+    class Meta:
+        model=User
+        fields=['username','email','password', 'password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    
+
+    def save(self, **kwargs):
+        user=User(
+            username=self.validated_data['username'],
+            email=self.validated_data['email']
+        )
+        password=self.validated_data['password']
+        password2=self.validated_data['password2']
+        if password !=password2:
+            raise serializers.ValidationError({"error":"password do not match"})
+        user.set_password(password)
+        user.is_teacher=True
+        user.save()
+        Teachers.objects.create(user=user)
+        return user
