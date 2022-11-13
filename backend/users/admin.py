d07f9be76f49c944700e9b61a8f70dfb4729aa6e
bmson7112 <sonm6976@gmail.com>
2022-11-13 21:13:51 +0700

    thêm quản lý user cho trang admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TeachersPerCourse, Students, Teachers
+from .models import *
 admin.site.register(Students)
 admin.site.register(Teachers)
 admin.site.register(TeachersPerCourse)
+admin.site.register(User)
