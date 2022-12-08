02befedc7a548bb930e1531dafffc58ff2edf397
Nguyen Tuan Nam <tuannamnguyen290602@gmail.com>
2022-12-08 13:31:35 +0700

    sửa setting để serve file

--- lms/settings.py
+++ lms/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,7 +129,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' 
+""" 
+This bit below is key - this provides a list of directories that
+Django will look into for additional static assets. In our case, we want Django to collect the static assets created by our frontend build process
+"""
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- lms/urls.py
+++ lms/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 admin.site.site_header  =  "LMS Administration"  
 admin.site.site_title  =  "LMS Administration"
@@ -13,5 +14,6 @@ urlpatterns = [
     path('api/courses/', include('courses.urls')),
     path('api/forum/', include('forum.urls')),
     path('api/', include('base.urls')),
+    path('', TemplateView.as_view(template_name="index.html"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
