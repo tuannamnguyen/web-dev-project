27c3b9a247e0f0092c05840a97cef1d27199f204
tuannamnguyen <tuannamnguyen290602@gmail.com>
2022-12-12 20:57:43 +0700

    fix url

--- backend/lms/urls.py
+++ backend/lms/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 from .static import static
 from django.views.generic import TemplateView
@@ -14,5 +14,6 @@ urlpatterns = [
     path('api/courses/', include('courses.urls')),
     path('api/forum/', include('forum.urls')),
     path('api/', include('base.urls')),
+    re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="index.html"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
